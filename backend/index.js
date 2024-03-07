f0bec6ed7a43c3cd81d3988cfdb0dbd6581dba0c
AzamCodes <azam.shaikh9105@gmail.com>
2024-03-07 17:59:41 +0530

    acallowcredentials

--- backend/index.js
+++ backend/index.js
@@ -29,6 +29,11 @@ app.use(
     credentials: true,
   })
 );
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
 app.use(express.json());
 app.use(cookieParser());
 app.use("/images", express.static(path.join(__dirname, "/images")));
