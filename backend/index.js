ff6d5248d5b607452f2b54b2eb42d5d0280f9e63
AzamCodes <azam.shaikh9105@gmail.com>
2024-03-07 18:04:52 +0530

    origin

--- backend/index.js
+++ backend/index.js
@@ -31,6 +31,14 @@ app.use(
 );
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://blogverse-omega.vercel.app"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   res.header("Access-Control-Allow-Credentials", "true");
   next();
 });
