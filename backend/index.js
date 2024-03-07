4a40a2ba2a2b323edb892964c0e32969edf20aa2
AzamCodes <azam.shaikh9105@gmail.com>
2024-03-07 19:39:48 +0530

    ddf

--- backend/index.js
+++ backend/index.js
@@ -27,22 +27,21 @@ app.use(
     origin: ["https://blogverse-omega.vercel.app"],
     methods: ["POST", "GET", "PUT", "DELETE", "HEAD", "OPTIONS"],
     credentials: true,
-    
   })
 );
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-//   res.header(
-//     "Access-Control-Allow-Origin",
-//     "https://blogverse-omega.vercel.app"
-//   );
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   res.header("Access-Control-Allow-Credentials", "true");
-//   next();
-// });
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://blogverse-omega.vercel.app"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
 app.use(express.json());
 app.use(cookieParser());
 app.use("/images", express.static(path.join(__dirname, "/images")));
